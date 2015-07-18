653febe422a21aabc589421f47c9bc8d5209e3fe
Harish <harish@localhost.localdomain>
2015-07-19 01:07:25 +0530

    Sirious Second commit

--- setup.py
+++ setup.py
@@ -26,4 +26,16 @@ if __name__ == '__main__':
         subprocess.call(["sudo", "apt-get","install","xclip"])
     install('bleach')
     install('Pillow')
-    subprocess.call(["sudo", "cp","Sirious","/usr/bin"])
+    subprocess.call(["sudo", "chmod","+x","Sirious"])
+    subprocess.call(["sudo", "cp","Sirious","/usr/local/bin/"])
+    print "\033c"
+    print """\nNow Sirious is a command in terminal (its Sirious not sirious) and to create a key binding for this command,
+\n GNOME:
+\n1. Go to System > Preferences(just Settings) > Keyboard >Keyboard Shortcuts
+\n2. Click the Add button or Custom Shortcuts (a plus sign)
+\n3. Choose a Name for your shortcut (can be just about anything)
+\n4. Give command as Sirious 
+\n5. Like other key binded provide a key by clicking on disabled and now it shows the name and its key-bind
+
+You can use any other method to create a key-bind for Sirious command
+"""
